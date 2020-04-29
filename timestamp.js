058952feb6d0ed38754196bca8aaf65483ff204c
anandv01 <anandv01@users.noreply.github.com>
2020-04-29 18:01:55 +0530

    time update

--- timestamp.js
+++ timestamp.js
@@ -1,4 +1,4 @@
 var d = new Date();
 var n = d.toDateString();
 var t = d.toLocaleTimeString();
-document.getElementById("demo").innerHTML = "Last update on "+n+", "+t+".";
+document.getElementById("time").innerHTML = "Last update on "+n+", "+t+".";
