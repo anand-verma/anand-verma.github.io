670d04ab2604afaee58b7684ce3e7228b22add13
Anand Kumar Verma <akv.vermaanand@gmail.com>
2024-08-16 10:34:38 +0530

    Updated Tags Handelling

--- Life/index.js
+++ Life/index.js
@@ -20,7 +20,7 @@ function renderBlogPosts(blogPosts) {
         blogCard.innerHTML = `
             ${imageSection}
             <div class="card-body">
-                <a href="./?page=${blog.fileName}" class="card-title h2">${blog.title}</a>
+                <a href="./index.html?page=${blog.fileName}" class="card-title h2">${blog.title}</a>
                 <p class="card-text">${blog.description}</p>
                 <p class="card-text">Tags: ${blog.tags.join(', ')}</p>
             </div>
@@ -44,7 +44,7 @@ function renderTags() {
 
     allTags.forEach((tag) => {
         const tagLink = document.createElement("a");
-        tagLink.href = `./?tag=${tag}`;
+        tagLink.href = `./index.html?tag=${tag}`;
         tagLink.textContent = `${tag} (${blogByTags[tag].length})`;
         tagLink.classList.add("btn", "btn-outline-primary", "w-100", "mb-2");
         tagLink.addEventListener("click", handleTagClick);
@@ -78,7 +78,7 @@ function handleTagDropdownChange(tag) {
 function updateTagSelection(tag) {
     const urlParams = new URLSearchParams(window.location.search);
     urlParams.set("tag", tag);
-    window.history.pushState({}, "", `./?tag=${tag}`);
+    window.history.pushState({}, "", `./index.html?tag=${tag}`);
     const filteredBlogs = blogByTags[tag] || [];
     renderBlogPosts(filteredBlogs);
 }
@@ -87,7 +87,7 @@ function updateTagSelection(tag) {
 function handleBlogClick(event) {
     event.preventDefault();
     const blogFile = event.target.getAttribute('href').split('=')[1];
-    window.history.pushState({}, "", `./?page=${blogFile}`);
+    window.history.pushState({}, "", `./index.html?page=${blogFile}`);
     fetchAndRenderBlog(blogFile);
 }
 
@@ -122,7 +122,7 @@ function handlePopState(event) {
 // Main function to orchestrate the process
 async function main() {
     try {
-        blogByTags = await getBlogByTags("index.md");
+        blogByTags = await getBlogByTags("./index.md");
         renderTags();
 
         // Handle initial rendering based on URL parameters
@@ -144,3 +144,119 @@ async function main() {
 }
 
 main();
+
+
+//-----------------------------------------------------------------------------------------------------
+// Function to fetch blog data from index.md
+async function fetchIndexData(indexMdPath) {
+    try {
+      const response = await fetch(indexMdPath);
+      const data = await response.text();
+      const frontMatter = extractFrontMatter(data);
+  
+      return frontMatter.blogs;
+    } catch (error) {
+      console.error('Error fetching index data:', error);
+      return [];
+    }
+  }
+  
+  async function populateBlogByTags(blogFiles) {
+    const blogByTags = {};
+    for (const file of blogFiles) {
+      try {
+        const rawData = await fetchBlogPostData(file);
+  
+        // Add blog data to blogByTags object based on tags
+        const { tags } = rawData;
+        for (const tag of tags) {
+          if (!blogByTags[tag]) {
+            blogByTags[tag] = [];
+          }
+          blogByTags[tag].push(rawData);
+        }
+  
+        // Add to 'All' tag
+        if (!blogByTags['All']) {
+          blogByTags['All'] = [];
+        }
+        blogByTags['All'].push(rawData);
+      } catch (error) {
+        console.error('Failed to load file:', error);
+      }
+    }
+    return blogByTags;
+  }
+  
+  // Function to fetch blog post data
+  async function fetchBlogPostData(filename) {
+    try {
+      const response = await fetch(filename);
+      const data = await response.text();
+      const frontData = extractFrontMatter(data);
+      const postData = parseFrontMatterData(filename, frontData);
+      return postData;
+    } catch (error) {
+      console.error('Error fetching blog post data:', error);
+    }
+  }
+  
+  function parseFrontMatterData(filename, frontData) {
+    const result = {};
+    result['fileName'] = filename;
+    result['title'] = frontData.title ? frontData.title : filename.split('.')[0];
+    result['description'] = frontData.description ? frontData.description : '';
+    result['imageUrl'] = frontData.imageurl ? frontData.imageurl : '';
+    result['date'] = frontData.date ? frontData.date : 'Undated';
+    result['tags'] = frontData.tags ? frontData.tags : ['Uncategorised'];
+  
+    return result;
+  }
+  
+  // Main function to orchestrate the process
+  async function getBlogByTags(indexMdPath) {
+    try {
+      const blogFiles = await fetchIndexData(indexMdPath);
+      const blogByTags = await populateBlogByTags(blogFiles);
+  
+      return blogByTags;
+      
+    } catch (error) {
+      console.error('Error in getBlogByTags function:', error);
+    }
+  }
+
+
+//-----------------------------------------------------------------------------------------------------
+
+function extractFrontMatter(mdContent) {
+    const frontMatterRegex = /^---([\s\S]*?)---/m;
+    const match = mdContent.match(frontMatterRegex);
+    
+    const frontMatter = {};
+    if (!match) {
+        return frontMatter; // Return null if no front matter is found
+    }
+   
+    const frontMatterContent = match[1].trim();
+    const frontMatterLines = frontMatterContent.split('\n');
+    
+    
+    let currentKey = null;
+    for (let line of frontMatterLines) {
+        line = line.split('#')[0];
+        line = line.trim();
+        
+        if (line.startsWith('- ')) {
+            frontMatter[currentKey] = frontMatter[currentKey] || [];
+            frontMatter[currentKey].push(line.slice(2).trim());
+            
+        } else if (line.includes(':')) {
+            const [key, value] = line.split(/:(.*)/);
+            currentKey = key.trim();
+            frontMatter[currentKey] = value ? value.trim() : '';
+        }
+    }
+    //console.log(match);
+    return frontMatter; // Return the front matter as a plain JavaScript object
+}
